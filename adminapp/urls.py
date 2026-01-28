8b5badf6b896adbf35d082000ff763f84eb153bd
HarshaSai1234 <harshap3712@gmail.com>
2026-01-28 08:47:42 +0530

    Updated 28-1-2025

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.adminapphomepage,name='adminapphomepage'),
+    path('printer/',views.printer,name='printer'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def adminapphomepage(request):
     return render(request,'adminapp/projecthomepage.html')
+def printer(request):
+    if request.method == "POST":
+        user_input = request.POST['klu']
+    a1={'klu' : user_input}
+    return render(request,'adminapp/printer.html',a1)
